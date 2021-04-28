d969cf8ab3197464805bf6ea5a72409df60f0cf4
Dspecial <1214302256@qq.com>
2021-04-28 17:50:08 +0800

    知识库接口

--- src/views/knowledge/Knowledge.vue
+++ src/views/knowledge/Knowledge.vue
@@ -1,52 +1,55 @@
 <template>
-  <div>
+  <div class="page-container">
     <!-- 登录信息 -->
     <global-tips></global-tips>
-    <!-- 列表 -->
-    <el-card>
-      <data-tables-server :data="tableData" layout="tool, table,pagination" :current-page="currentPage" :page-size="pageSize" 
-			:pagination-props="{ background: true, pageSizes: [15,30,45,60], total: total }" @query-change="loadData" :filters="filters" :table-props="tableProps">
-        <div class="mb-3" slot="tool">
-          <h4 class="fs_16 font-weight-semibold m-0 text-000 mb-3">知识库列表</h4>
-          <div class="d-flex align-items-center project_search_div">
-          	<div class="d-flex align-items-center">
-          		<el-input
-    				    placeholder="请输入标题、内容"
-    				    prefix-icon="el-icon-search"
-    				    v-model="filters[0].value"
-                class="mr-3">
-    				  </el-input>
-              <el-cascader
-                v-model="filters[1].value"
-                placeholder="请选择分类"
-                :options="cateOptions"
-                :props="cateProps"
-                class="mr-3">
-              </el-cascader>
-              <el-date-picker
-                v-model="filters[2].value"
-                type="daterange"
-                range-separator="至"
-                start-placeholder="发送时间"
-                end-placeholder="发送时间"
-                align="right"
-                value-format="yyyy-MM-dd">
-              </el-date-picker>
-          	</div>
-          </div>
+    <el-container class="second-container">
+      <el-aside width="200px" class="second-aside mr-3">
+        <div class="pt-3 pb-3">
+          <el-tree :data="cateTree" :props="defaultProps" @node-click="handleNodeClick" :default-expand-all="true"></el-tree>
         </div>
-        <el-table-column type="index" :index="indexMethod" label="序号" width="50"></el-table-column>
-        <el-table-column prop="title" label="消息标题" width="300"></el-table-column>
-        <el-table-column prop="desc" label="简介"></el-table-column>
-        <el-table-column prop="parent_str" label="所属分类"></el-table-column>
-				<el-table-column prop="createtime" label="发送时间"></el-table-column>
-        <el-table-column fixed="right" label="操作" width="150" align="center">
-          <template slot-scope="scope">
-            <span class="text-primary cursor-pointer" @click="knowledgeDetail(scope.$index,scope.row)">查看详情</span>
-          </template>
-        </el-table-column>
-      </data-tables-server>
-    </el-card>
+      </el-aside>
+      <el-main class="p-0">
+        <!-- 列表 -->
+        <el-card>
+          <data-tables-server :data="tableData" layout="tool, table,pagination" :current-page="currentPage" :page-size="pageSize" 
+          :pagination-props="{ background: true, pageSizes: [15,30,45,60], total: total }" @query-change="loadData" :filters="filters" :table-props="tableProps">
+            <div class="mb-3" slot="tool">
+              <h4 class="fs_16 font-weight-semibold m-0 text-000 mb-3">知识库列表</h4>
+              <div class="d-flex align-items-center project_search_div">
+                <div class="d-flex align-items-center">
+                  <el-input
+                    placeholder="请输入标题、内容"
+                    prefix-icon="el-icon-search"
+                    v-model="filters[0].value"
+                    class="mr-3">
+                  </el-input>
+                  <el-date-picker
+                    v-model="filters[1].value"
+                    type="daterange"
+                    range-separator="至"
+                    start-placeholder="发送时间"
+                    end-placeholder="发送时间"
+                    align="right"
+                    value-format="yyyy-MM-dd">
+                  </el-date-picker>
+                </div>
+              </div>
+            </div>
+            <el-table-column type="index" :index="indexMethod" label="序号" width="50"></el-table-column>
+            <el-table-column prop="title" label="消息标题" width="300"></el-table-column>
+            <el-table-column prop="desc" label="简介"></el-table-column>
+            <el-table-column prop="parent_str" label="所属分类"></el-table-column>
+            <el-table-column prop="createtime" label="发送时间"></el-table-column>
+            <el-table-column fixed="right" label="操作" width="150" align="center">
+              <template slot-scope="scope">
+                <span class="text-primary cursor-pointer" @click="knowledgeDetail(scope.$index,scope.row)">查看详情</span>
+              </template>
+            </el-table-column>
+          </data-tables-server>
+        </el-card>
+      </el-main>
+    </el-container>
+    
   </div>
 </template>
 
@@ -87,11 +90,13 @@
         currentPage: 1, //当前页
         pageSize: 15, //每页显示条数
 				cateOptions:[],
-        cateProps:{
+        cateTree:[],
+        defaultProps:{
           value:"id",
           label:"cate_name",
           children:"children"
         },
+        currentNode:"",
       }
     },
     computed: {
@@ -108,9 +113,15 @@
       initCate(){
         this.$api.knowledgeCate({}).then(data => {
           if(data.code == 0){
-            this.cateOptions = data.data;
+            // this.cateOptions = data.data;
+            this.cateTree = data.data;
           }
         });
+      },
+      // 点击分类
+      handleNodeClick(data) {
+        this.currentNode = data.id;
+        this.loadData();
       },
       // 加载数据
       loadData(queryInfo) { 
@@ -122,9 +133,9 @@
         this.$api.knowledgeList({
           page:this.currentPage,
           limit:this.pageSize,
+					cate_id:this.currentNode,
           keywords:this.filters[0].value,
-					cate_id:this.filters[1].value[this.filters[1].value.length - 1],
-          createtime:this.filters[2].value?this.filters[2].value.join(" - "):'',
+          createtime:this.filters[1].value?this.filters[1].value.join(" - "):'',
         }).then(data=>{
           if(data.code == 0){
             this.total = data.data.total;
@@ -133,6 +144,10 @@
             this.$message.error(data.msg);
           }
         });
+      },
+      
+      initData(){
+
       },
       // 查看详情
       knowledgeDetail(index,row){

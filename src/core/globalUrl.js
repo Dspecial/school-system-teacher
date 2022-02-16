9da861ff1db62989f78b979c2b070b5c39a6a5d6
Dspecial <1214302256@qq.com>
2022-02-16 17:02:41 +0800

    修改

--- src/core/globalUrl.js
+++ src/core/globalUrl.js
@@ -3,7 +3,7 @@
  * @Email: dxxtalking@163.com
  * @Date: 2020-12-24 09:09:03
  * @LastEditors: dxx
- * @LastEditTime: 2021-08-06 17:35:46
+ * @LastEditTime: 2022-02-16 16:59:29
  */
 
 // const baseURL = 'http://xmgl.ccit.js.cn'

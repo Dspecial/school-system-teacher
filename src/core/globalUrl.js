4f31e2422f6d2276db575fbfbb690456f4774d21
Dspecial <1214302256@qq.com>
2022-06-06 18:11:20 +0800

    url

--- src/core/globalUrl.js
+++ src/core/globalUrl.js
@@ -3,7 +3,7 @@
  * @Email: dxxtalking@163.com
  * @Date: 2020-12-24 09:09:03
  * @LastEditors: dxx
- * @LastEditTime: 2022-05-25 18:22:15
+ * @LastEditTime: 2022-06-06 18:08:03
  */
 
 // const baseURL = 'http://xmgl.ccit.js.cn'

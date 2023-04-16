f7a319d5f6386605cfd2d2bdc2f3719261a2fd52
dingxixi <dingxixi@techbloom.net>
2023-04-16 22:22:41 +0800

    提交

--- src/components/GlobalTips.vue
+++ src/components/GlobalTips.vue
@@ -1,66 +1,75 @@
+<!--
+ * @Author: dxx
+ * @Date: 2022-02-16 15:38:46
+ * @LastEditors: dxx
+ * @LastEditTime: 2023-04-06 10:47:59
+-->
 <template>
-	<div class="globalTips mb-3 pt-2 pb-2 pl-3 pr-3">
-		<div class="d-flex align-items-center">
-			<i class="el-icon-warning mr-2"></i>
-			<span>欢迎使用{{systemName}}！姓名：{{userName}}  当前时间：{{time}}</span>
-		</div>
-	</div>
+  <div class="globalTips mb-3 pt-2 pb-2 pl-3 pr-3">
+    <div class="d-flex align-items-center">
+      <i class="el-icon-warning mr-2"></i>
+      <span>欢迎使用{{ systemName }}！姓名：{{ userName }} 当前时间：{{ time }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'GlobalTips',
-		data () {
-			return {
-				systemName:'管理后台',
-				userId:"1234",
-				userName:"张三",
-				time:"2020-10-10 12:00:00",
-			}
-		},
-		components: {},
-		mounted(){
-			this.loginConfig();
-			this.userId = this.VueCookies.get('application_userId');
-			this.userName = this.VueCookies.get('application_userName');
-			this.time = this.getDayjsTime();
-		},
-		methods:{
-			// 获取系统名称
-      loginConfig(){
-        this.$api.loginConfig({
-        }).then(data=>{
-          if(data.code == 0){
-            this.systemName = data.data.application_name;
-          }else{
-            const h = this.$createElement;
-            this.$notify({
-              title: "获取失败",
-              message: h('i', {
-                style: 'color: teal'
-              }, data.msg),
-              type: 'warning',
-              duration: 3000,
-            });
-          }
-        })
-      },
-			getDayjsTime () {
-	      let dayjsTime = this.$moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
-	      return dayjsTime
-	    }
-		},
-	}
+export default {
+  name: 'GlobalTips',
+  data() {
+    return {
+      systemName: '管理后台',
+      userId: '1234',
+      userName: '张三',
+      time: '2020-10-10 12:00:00',
+    };
+  },
+  components: {},
+  mounted() {
+    this.loginConfig();
+    this.userId = this.VueCookies.get('application_userId');
+    this.userName = this.VueCookies.get('application_userName');
+    this.time = this.getDayjsTime();
+  },
+  methods: {
+    // 获取系统名称
+    loginConfig() {
+      this.$api.loginConfig({}).then(data => {
+        if (data.code == 0) {
+          this.systemName = data.data.application_name;
+        } else {
+          const h = this.$createElement;
+          this.$notify({
+            title: '获取失败',
+            message: h(
+              'i',
+              {
+                style: 'color: teal',
+              },
+              data.msg,
+            ),
+            type: 'warning',
+            duration: 3000,
+          });
+        }
+      });
+    },
+    getDayjsTime() {
+      let dayjsTime = this.$moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+      return dayjsTime;
+    },
+  },
+};
 </script>
 
 <style>
-	.globalTips{
-		background: #EDF5FF;
-		border: 1px solid #97C3FF;
-		border-radius: 4px;
-	}
-	.globalTips i {
-		color: #1677FF;
-		font-size: 18px;
-	}
+.globalTips {
+  background: #edf5ff;
+  border: 1px solid #97c3ff;
+  border-radius: 4px;
+}
+.globalTips i {
+  color: #1677ff;
+  font-size: 18px;
+}
 </style>
